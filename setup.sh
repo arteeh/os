7095917af38ed56c1c47ac178b89e97cfb26029b
Maarten de Jong <arteeh@vm.vm>
2020-07-29 14:41:54 +0200

    fix bashrc write inside stuff folder

--- setup.sh
+++ setup.sh
@@ -5,14 +5,14 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-echo "alias i='sudo apt install -y'" >> .bashrc
-echo "alias r='sudo apt purge -y'" >> .bashrc
-echo "alias l='clear;apt list --installed'" >> .bashrc
-echo "alias u='sudo apt update'" >> .bashrc
-echo "alias d='sudo apt dist-upgrade -y'" >> .bashrc
-echo "alias a='sudo apt autoremove --purge -y'" >> .bashrc
-echo "alias c='sudo apt clean'" >> .bashrc
-echo "alias udac='u;d;a;c'" >> .bashrc
+echo "alias i='sudo apt install -y'" >> /home/arteeh/.bashrc
+echo "alias r='sudo apt purge -y'" >> /home/arteeh/.bashrc
+echo "alias l='clear;apt list --installed'" >> /home/arteeh/.bashrc
+echo "alias u='sudo apt update'" >> /home/arteeh/.bashrc
+echo "alias d='sudo apt dist-upgrade -y'" >> /home/arteeh/.bashrc
+echo "alias a='sudo apt autoremove --purge -y'" >> /home/arteeh/.bashrc
+echo "alias c='sudo apt clean'" >> /home/arteeh/.bashrc
+echo "alias udac='u;d;a;c'" >> /home/arteeh/.bashrc
 
 echo "deb http://deb.debian.org/debian unstable main contrib non-free" > /etc/apt/sources.list
 
