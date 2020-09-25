dab3a7f867d06ec445cb032c77f338f49c0ce69b
Maarten de Jong <arteeh@silver.silver>
2020-09-25 23:16:42 +0200

    apps

--- linux2.sh
+++ linux2.sh
@@ -6,7 +6,7 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Get rid of trash
-apt purge -y popularity-contest vim-common vim-tiny firefox-esr yelp im-config eog baobab evince totem gnome-calculator gnome-characters gnome-contacts gnome-disk-utility gnome-font-viewer gnome-logs gnome-shell-extensions gnome-software gnome-sushi gnome-system-monitor gnome-tweaks
+apt purge -y popularity-contest vim-common vim-tiny firefox-esr yelp im-config eog baobab evince totem gnome-calculator gnome-characters gnome-contacts gnome-disk-utility gnome-font-viewer gnome-logs gnome-shell-extension* gnome-software gnome-sushi gnome-system-monitor gnome-tweaks
 apt autoremove --purge -y
 
 # Fix network so networkmanager can take control
@@ -17,4 +17,5 @@ sudo gedit /etc/default/grub
 update-grub
 
 # Get my apps
-flatpak install flathub -y org.gnome.Calculator org.gnome.clocks org.gnome-Calendar org.gnome.eog org.gnome.Geary org.gnome.Totem org.glimpse_editor.Glimpse org.libreoffice.LibreOffice org.qbittorrent.qBittorrent org.mozilla.firefox org.gnome.Lollypop
+flatpak install flathub -y org.gnome.Calculator org.gnome.Clocks org.gnome.Contacts org.gnome-Calendar org.gnome.eog org.gnome.Totem
+flatpak install flathub -y org.glimpse_editor.Glimpse org.libreoffice.LibreOffice org.qbittorrent.qBittorrent org.mozilla.firefox
