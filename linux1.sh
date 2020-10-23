cb0ac9e5ff2f949aeb033f53bc3c624ac623ad80
Maarten de Jong <accounts@arteeh.com>
2020-10-23 10:41:01 +0200

    Gedit plugins

--- linux1.sh
+++ linux1.sh
@@ -46,7 +46,7 @@ apt clean
 # apt install -y firmware-linux firmware-iwlwifi
 
 # Install the desktop
-apt install -y plymouth plymouth-themes flatpak gnome-core fonts-noto
+apt install -y plymouth plymouth-themes flatpak gnome-core fonts-noto gedit-plugin-color-picker gedit-plugin-text-size
 
 # Set up plymouth
 plymouth-set-default-theme -R spinner
