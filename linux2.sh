67ebd3353e714627b1bbf542cb67f776d177a702
Maarten de Jong <accounts@arteeh.com>
2020-10-23 11:41:32 +0200

    Traslator app and reordering and tidying

--- linux2.sh
+++ linux2.sh
@@ -5,11 +5,6 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-# Get rid of trash
-apt purge -y popularity-contest vim-common vim-tiny needrestart malcontent firefox-esr yelp im-config eog baobab evince totem gnome-calculator gnome-characters gnome-contacts gnome-disk-utility gnome-font-viewer gnome-logs gnome-shell-extension* gnome-software gnome-sushi gnome-system-monitor gnome-tweaks
-
-apt autoremove --purge -y
-
 # Disable built in network so networkmanager can take control
 sudo gedit /etc/network/interfaces
 
@@ -18,8 +13,51 @@ sudo gedit /etc/default/grub
 
 update-grub
 
-# Get Gnome apps
-flatpak install flathub -y org.gnome.Calculator org.gnome.Clocks org.gnome.Contacts org.gnome.Calendar org.gnome.eog org.gnome.Totem org.gnome.Boxes org.gnome.Maps org.gnome.Photos org.gnome.Fractal
+# Get rid of trash
+apt purge -y \
+	popularity-contest \
+	vim-common \
+	vim-tiny \
+	needrestart \
+	malcontent \
+	firefox-esr \
+	yelp \
+	im-config \
+	eog \
+	baobab \
+	evince \
+	totem \
+	gnome-calculator \
+	gnome-characters \
+	gnome-contacts \
+	gnome-disk-utility \
+	gnome-font-viewer \
+	gnome-logs \
+	gnome-shell-extension* \
+	gnome-software \
+	gnome-sushi \
+	gnome-system-monitor \
+	gnome-tweaks
 
-# Get third party apps
-flatpak install flathub -y org.mozilla.firefox org.mozilla.Thunderbird org.glimpse_editor.Glimpse org.libreoffice.LibreOffice com.transmissionbt.Transmission com.discordapp.Discord com.microsoft.Teams
+apt autoremove --purge -y
+
+# Get apps
+flatpak install flathub -y \
+	org.mozilla.firefox \
+	org.mozilla.Thunderbird \
+	org.gnome.Boxes \
+	org.gnome.Calculator \
+	org.gnome.Calendar \
+	org.gnome.Clocks \
+	org.gnome.Contacts \
+	org.gnome.Fractal \
+	org.gnome.Maps \
+	org.gnome.Photos \
+	com.github.gi_lom.dialect \
+	org.glimpse_editor.Glimpse \
+	com.transmissionbt.Transmission \
+	com.discordapp.Discord \
+	com.microsoft.Teams \
+	org.libreoffice.LibreOffice \
+	org.gnome.eog \
+	org.gnome.Totem
